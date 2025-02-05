65ca72904aa8a7775143b5dcc3a1041746700a39
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-04 20:02:54 -0400

    add custom store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './style.css';
 import { App } from './todos/app';
 
-console.log('Hola mundo')
+import todoStore from './store/todo.store';
+
+console.log(todoStore.initStore())
 
 App('#app');
---
+++ src/store/todo.store.js
@@ -0,0 +1,28 @@
+import { Todo } from "../todos/models/todo.model";
+
+const Filters = {
+    All: 'all',
+    Completed: 'completed',
+    Pending: 'pending'
+}
+
+const state = {
+    todo: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del tiempo'),
+        new Todo('Piedra del infinito'),
+    ],
+    filter: Filters.All
+}
+
+const initStore = () => {
+    console.log(state);
+    console.log('Init Store 🥑');
+
+
+
+}
+
+export default {
+    initStore
+}
