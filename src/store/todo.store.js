2f6cf9e83d894a832cba64eb5991a754ea95f6c0
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-05 14:20:33 -0400

    store functions

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -18,11 +18,62 @@ const state = {
 const initStore = () => {
     console.log(state);
     console.log('Init Store 🥑');
-
-
-
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * 
+ * @param {String} todoId Todo identifier
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+
+/**
+ * 
+ * @param {String} todoId Todo identifier 
+ */
+const deleteTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
+
 export default {
-    initStore
+    initStore,
+    loadStore,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
+    addTodo,
+    toggleTodo
 }
