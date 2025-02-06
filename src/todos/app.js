33d59ab26ca268d846d3463e458681c1e8d691b6
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-05 23:28:08 -0400

    feat: reset input description

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,5 +1,5 @@
 import todoStore from '../store/todo.store';
-import html from './app.html?raw'; 
+import html from './app.html?raw';
 import { renderTodos } from './use-cases';
 
 const ElementIDs = {
@@ -15,7 +15,7 @@ const ElementIDs = {
 export const App = (elementId) => {
 
     const displayTodos = () => {
-        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
         renderTodos(ElementIDs.TodoList, todos);
     }
     // Cuando la funcion App() se llama
@@ -31,11 +31,12 @@ export const App = (elementId) => {
 
     // Listeners
     newDescriptionInput.addEventListener('Keyup', (event) => {
-        if(event.keyCode !== 13) return;
-        if(event.target.value.trim().length === 0 ) return;
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0) return;
 
-        todoStore.addTodo( event.target.value );
+        todoStore.addTodo(event.target.value);
         displayTodos();
+        event.target.value = '';
     });
 
 }
