44677b0ed767c4d1027030afb60248cf0a4592ec
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-05 23:08:59 -0400

    add functionality to the use cases

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -4,13 +4,25 @@ import { Todo } from "../models/todo.model";
  * 
  * @param {Todo} todo 
  */
-export const createTodoHTML = ( todo ) => {
-    if(!todo) throw new Error('A Todo object is required');
+export const createTodoHTML = (todo) => {
+    if (!todo) throw new Error('A Todo object is required');
 
-    const html = ``
+    const { description, done, id } = todo;
+
+    const html = `
+        <div class="view">
+            <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+            <label> ${ description } </label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    `
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', id);
+
+    if (done) liElement.classList.add('completed');
 
     return liElement;
 
